54649b6f40447e813945323ff7293f59ebefb744
Moises Romo <moisesrc@mx1.ibm.com>
2024-12-28 11:50:18 -0600

    iolink rest

--- rel_ros_master_control/rel_ros_master_control/rest/router.py
+++ rel_ros_master_control/rel_ros_master_control/rest/router.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
+from fastapi import APIRouter, HTTPException, Request, Response, status
+from pydantic import BaseModel
 
+from rel_ros_master_control.control import RelControl
 from rel_ros_master_control.logger import new_logger
 
 logger = new_logger(__name__)
 
 api_router = APIRouter()
+
+
+class WriteRequest(BaseModel):
+    register: int
+    value: int
 
 
 @api_router.get("/")
@@ -16,3 +23,19 @@ def read_root():
 def health():
     """This is a generic endpoint to check the service is alive"""
     return "OK"
+
+
+@api_router.post("/read/{register}")
+async def get_dispense_registers_data(
+    request: Request,
+    register: int,
+):
+    logger.debug("getting register %s data", register)
+    control: RelControl = request.app.state.control
+    data = control.read_holding_register(register)
+    if error := data.get("error"):
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error getting register data from modbus slave: {error}",
+        )
+    return data
