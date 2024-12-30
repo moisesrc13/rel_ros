027f3d9e034dd8206f40b57c9b7f59f8a669182f
Moises Romo <moisesrc@mx1.ibm.com>
2024-12-30 16:20:33 -0600

    iolink interface data

--- run-ros-build-interfaces.sh
+++ run-ros-build-interfaces.sh
@@ -3,5 +3,7 @@ source ~/.bashrc
 cd ~/ros2_ws
 colcon build --packages-select rel_interfaces
 source install/setup.bash
+echo "------------------ HMI"
 ros2 interface show rel_interfaces/msg/HMI
+echo "------------------ IOLinkData"
 ros2 interface show rel_interfaces/msg/IOLinkData
