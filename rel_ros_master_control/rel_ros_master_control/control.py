c37f495adde36df5a2018095b710d94963e88a8d
Moises Romo <moisesrc@mx1.ibm.com>
2024-12-30 15:09:31 -0600

    applyoffset

--- rel_ros_master_control/rel_ros_master_control/control.py
+++ rel_ros_master_control/rel_ros_master_control/control.py
@@ -58,13 +58,18 @@ class RelControl:
         self.master_io_link.do_connect()
         logger.info("master_io_link connected .✨")
 
+    def get_register_with_offset(self, register: int) -> int:
+        register = register + self.master_io_link.slave.offset
+        logger.debug("register with offset %s", register)
+        return register
+
     def read_device_port_input_status(self, port: str) -> int:
         port: DevicePort = getattr(self.master_io_link.slave.device_ports, port)
         logger.debug(
             "port_num %s status_register %s", port, port.holding_registers.data_input_status
         )
         rr = self.master_io_link.slave_conn.read_holding_registers(
-            address=port.holding_registers.data_input_status.address,
+            address=self.get_register_with_offset(port.holding_registers.data_input_status.address),
             count=port.holding_registers.data_input_status.words,
         )
         decoder = get_decoder(rr)
@@ -73,7 +78,7 @@ class RelControl:
     def read_device_port_register(self, register: Register) -> int:
         logger.debug("read device register %s", register)
         rr = self.master_io_link.slave_conn.read_holding_registers(
-            address=register.address,
+            address=self.get_register_with_offset(register.address),
             count=register.words,
         )
         decoder = get_decoder(rr)
@@ -82,7 +87,7 @@ class RelControl:
     def write_device_port_register(self, register: Register, value: int) -> int:
         logger.debug("write device register %s", register)
         response = self.master_io_link.slave_conn.write_register(
-            address=register.address, value=value
+            address=self.get_register_with_offset(register.address), value=value
         )
         if response.isError():
             logger.error("error writing register")
@@ -92,7 +97,9 @@ class RelControl:
     def write_holding_register(self, register: int, value: int) -> ControlStatus:
         status = ControlStatus()
         logger.info("writing to register %s value %s", register, value)
-        response = self.master_io_link.slave_conn.write_register(address=register, value=value)
+        response = self.master_io_link.slave_conn.write_register(
+            address=self.get_register_with_offset(register), value=value
+        )
         if response.isError():
             logger.error("error writing register")
             status.error = response
@@ -105,7 +112,9 @@ class RelControl:
     def read_holding_register(self, register: int) -> ControlStatus:
         status = ControlStatus()
         logger.info("reading register %s", register)
-        response = self.master_io_link.slave_conn.read_holding_registers(address=register, count=1)
+        response = self.master_io_link.slave_conn.read_holding_registers(
+            address=self.get_register_with_offset(register), count=1
+        )
         if response.isError():
             logger.error("error reading register")
             status.error = response
