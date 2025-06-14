f2eb1aa94b96a2482dff07128af88ab1be455362
Moises Romo <moisesrc@mx1.ibm.com>
2025-06-14 12:36:23 -0600

    tester

---
+++ rel_ros_master_control/rel_ros_master_control/control_tester.py
@@ -0,0 +1,38 @@
+import argparse
+from enum import Enum
+
+from rel_ros_master_control.config import load_hmi_config, load_iolink_config
+from rel_ros_master_control.control import RelControl
+from rel_ros_master_control.logger import new_logger
+
+logger = new_logger(__name__)
+
+
+class TestCase(Enum):
+    A = "a"  # A) Sensor laser d<Z
+    B = "b"  # B) Sensor laser d>Z && d<=Y
+    C = "c"  # C) Sensor laser d>Y && d<=X
+    D = "d"  # D) Sensor laser d>X && d<W
+    E = "e"  # E) Sensor laser d>W && d<= ∞
+
+
+if __name__ == "__main__":
+    logger.info("starting control tester for node id 0 🤠")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--testcase",
+        help="test case",
+        dest="testcase",
+        default="a",
+        type=str,
+    )
+    iolink_config = load_iolink_config()
+    hmi_config = load_hmi_config()
+    control = RelControl(
+        iolink_slave=iolink_config.iolinks[0],
+        iolink_hr=iolink_config.holding_registers,
+        hmi_slave=hmi_config.hmis[0],
+        hmi_hr=hmi_config.holding_registers,
+        hmi_cr=hmi_config.coil_registers,
+    )
