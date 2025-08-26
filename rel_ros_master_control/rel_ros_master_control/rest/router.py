0bebded0c1397d2cc21d8b257b679782393c2ddc
moisesrc13 <jm.roco@gmail.com>
2025-08-25 18:14:08 -0600

    docs for commands

--- rel_ros_master_control/rel_ros_master_control/rest/router.py
+++ rel_ros_master_control/rel_ros_master_control/rest/router.py
@@ -18,6 +18,12 @@ class WriteRequest(BaseModel):
     slave_type: SlaveType = SlaveType("hmi")
 
 
+class ReadRequest(BaseModel):
+    register: int
+    register_type: RegisterType = RegisterType("coil")
+    slave_type: SlaveType = SlaveType("hmi")
+
+
 class PWMRequest(BaseModel):
     frequency: int = 1000
     time_seconds: int = 10
@@ -35,14 +41,32 @@ def health():
     return "OK"
 
 
-@api_router.get("/modbus/read/{register}")
+@api_router.post("/control/read")
 async def read_register(
     request: Request,
-    register: int,
+    read_request: ReadRequest,
 ):
-    logger.debug("getting register %s data", register)
+    logger.debug(
+        "getting register %s data for slave type %s and register type",
+        read_request.register,
+        read_request.slave_type,
+        read_request.register_type,
+    )
     control: RelControl = request.app.state.control
-    control_status = control.read_iolink_hregister(register)
+
+    if read_request.slave_type == SlaveType.IOLINK:
+        if read_request.register == 0:
+            return control.get_iolink_hr_data()
+        control_status = control.read_iolink_hregister(read_request.register)
+    else:
+        if read_request.register == 0 and read_request.register_type == RegisterType.HOLDING:
+            return control.get_hmi_hr_data()
+        if read_request.register == 0 and read_request.register_type == RegisterType.COIL:
+            return control.get_hmi_cr_data()
+        else:
+            control_status = control.read_hmi_register(
+                read_request.register, rtype=read_request.register_type
+            )
     if control_status.error:
         raise HTTPException(
             status_code=control_status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -51,7 +75,7 @@ async def read_register(
     return control_status.model_dump()
 
 
-@api_router.post("/modbus/write")
+@api_router.post("/control/write")
 async def write_register(
     request: Request,
     write_request: WriteRequest,
@@ -64,20 +88,20 @@ async def write_register(
     )
     control: RelControl = request.app.state.control
     if write_request.slave_type == SlaveType.IOLINK:
-        control_status = control.write_iolink_hregister(write_request.register, write_request.value)
+        modebus_status = control.write_iolink_hregister(write_request.register, write_request.value)
     else:
-        control_status = control.write_register(
+        modebus_status = control.write_register(
             register=write_request.register,
             value=write_request.value,
             stype=write_request.slave_type,
             rtype=write_request.register_type,
         )
-    if control_status.error:
+    if modebus_status.error:
         raise HTTPException(
-            status_code=control_status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error getting register data: {control_status.error}",
+            status_code=modebus_status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error getting register data: {modebus_status.error}",
         )
-    return control_status.model_dump()
+    return modebus_status.model_dump()
 
 
 @api_router.post("/pwm/run")
